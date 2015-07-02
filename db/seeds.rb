7327e6e2bd1cedb5a7c77c7a270c4932d11328c9
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-03 11:42:33 +1200

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,2 @@
-Cell.destroy_all
-10.times do |y|
-	10.times do |x|
-		Cell.create(x:x+1, y:y+1, food_count:rand(1..10), mineral_count:rand(1..10))
-	end
-end
+Cell.starting_cells
+Cell.assign_and_subtract_from_total_resources
