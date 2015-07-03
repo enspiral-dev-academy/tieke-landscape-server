41e22a0284ecbfb21394083f953732699e2ed96c
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-03 16:05:21 +1200

    changed seed file to use less resources

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -16,20 +16,21 @@ class Cell < ActiveRecord::Base
 
   def self.assign_random_food
     total_landscape_food = rand(1000..1500)
+    food_amount_array = Array.new(100){0}
     total_landscape_food.times do
-      cell = Cell.all.sample
-      cell.food_count += 1
-      cell.save!
+      food_amount_array[rand(0..99)] += 1
     end
+    Cell.all.each {|cell| cell.update(food_count: food_amount_array.shift)}
   end
 
   def self.assign_random_minerals
     total_landscape_minerals = rand(3000..4000)
+    mineral_amount_array = Array.new(100){0}
     total_landscape_minerals.times do
-      cell = Cell.all.sample
-      cell.mineral_count += 1
-      cell.save!
-    end
+        mineral_amount_array[rand(0..99)] += 1
+      end
+      p mineral_amount_array
+      Cell.all.each {|cell| cell.update(mineral_count: mineral_amount_array.shift)}
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
+start = Time.now
+
 Cell.starting_cells
 Cell.assign_and_subtract_from_total_resources
+
+ends = Time.now
+
+p ends - start
